af537769534c4d29769dab14c74e84a216f1df70
aniket kumar <anikush0620@gmail.com>
2021-08-02 21:02:39 +0530

    checksubstringpresentingivenstring.py

---
+++ checksubstringpresentingivenstring.py
@@ -0,0 +1,10 @@
+
+def check(string, sub_str):
+    if (string.find(sub_str) == -1):
+        print("NO")
+    else:
+        print("YES")
+
+string = "ani for ani"
+sub_str = "ani"
+check(string, sub_str)
