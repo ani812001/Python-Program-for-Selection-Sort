7fdd45a73853812854139f99dc9ab116178a1da7
aniket kumar <anikush0620@gmail.com>
2021-08-02 20:12:28 +0530

    reversewordinstring.py

---
+++ reversewordinstring.py
@@ -0,0 +1,13 @@
+
+def rev_sentence(sentence):
+
+	words = sentence.split(' ')
+
+	reverse_sentence = ' '.join(reversed(words))
+
+	return reverse_sentence
+
+if __name__ == "__main__":
+	input = 'my name is aniket'
+	print (rev_sentence(input))
+
