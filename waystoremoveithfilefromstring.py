dc11652e26d81e1e1d16cd6ef3501b45351e4557
aniket kumar <anikush0620@gmail.com>
2021-08-02 20:34:53 +0530

    waystoremoveithfilefromstring.py

---
+++ waystoremoveithfilefromstring.py
@@ -0,0 +1,12 @@
+
+test_str = "haibuddy"
+
+print ("The original string is : " + test_str)
+
+new_str = ""
+
+for i in range(len(test_str)):
+	if i != 2:
+		new_str = new_str + test_str[i]
+
+print ("The string after removal of i'th character : " + new_str)
